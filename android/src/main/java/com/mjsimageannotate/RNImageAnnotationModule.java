050258471e8cefac46a40ede7cccfbb87315c07a
Mike MacDonald <mikem@mjs-consulting.biz>
2018-08-20 17:16:45 -0400

    add configuration attributes for image size and quality

--- android/src/main/java/com/mjsimageannotate/RNImageAnnotationModule.java
+++ android/src/main/java/com/mjsimageannotate/RNImageAnnotationModule.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.util.Base64;
 import android.util.Log;
 
@@ -15,6 +16,9 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.io.ByteArrayOutputStream;
 
 public class RNImageAnnotationModule extends ReactContextBaseJavaModule {
 
@@ -37,21 +41,52 @@ public class RNImageAnnotationModule extends ReactContextBaseJavaModule {
       byte[] decodedString = Base64.decode(image64, Base64.DEFAULT);
       Bitmap originalBitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
-      Bitmap newBitmap = Bitmap.createBitmap(originalBitmap.getWidth(), originalBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+      int width = originalBitmap.getWidth();
+      int height = originalBitmap.getHeight();
+      int quality = 80;
+      int fontSize = 20;
+
+      boolean rescale = false;
+
+      if (config.hasKey("outputMaxWidth")){
+        width = config.getInt("outputMaxWidth");
+        rescale = true;
+      }
+      if (config.hasKey("outputMaxHeight")){
+        height = config.getInt("outputMaxHeight");
+        rescale = true;
+      }
+      if (rescale){
+        if (originalBitmap.getWidth()> originalBitmap.getHeight()){
+          height = (width * originalBitmap.getHeight()) / originalBitmap.getWidth();
+        } else {
+          width = (height * originalBitmap.getWidth()) / originalBitmap.getHeight();
+        }
+
+      }
+
+
+      Rect src = new Rect(0,0,originalBitmap.getWidth(), originalBitmap.getHeight());
+      Rect dest = new Rect(0,0, width, height);
+
+
+
+      Bitmap newBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
       Canvas canvas = new Canvas(newBitmap);
 
       Paint paint = new Paint();
       paint.setColor(Color.WHITE); // Text Color
-      paint.setTextSize(30); // Text Size
+      paint.setTextSize(fontSize); // Text Size
       paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER)); // Text Overlapping Pattern
       paint.setShadowLayer(3,0,2, Color.BLACK);
 
-      canvas.drawBitmap(originalBitmap, 0, 0, paint);
+
+      canvas.drawBitmap(originalBitmap, src, dest, paint);
       canvas.drawText(text, 10, 30, paint);
 
 
       ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-      newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream);
+      newBitmap.compress(Bitmap.CompressFormat.JPEG, quality, byteArrayOutputStream);
       byte[] byteArray = byteArrayOutputStream .toByteArray();
       output = Base64.encodeToString(byteArray, Base64.DEFAULT);
 
--- ios/RNImageAnnotation.m
+++ ios/RNImageAnnotation.m
@@ -23,18 +23,40 @@ RCT_EXPORT_METHOD(annotate:(NSString *)image64
     
     UIImage *inImage = [UIImage imageWithData:inData];
     
-    NSDictionary *shadowFontAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIFont fontWithName:@"Arial" size:40], NSFontAttributeName,[UIColor colorWithRed:0 green:0 blue:0 alpha:1],NSForegroundColorAttributeName, nil];
+    id fsv = [config objectForKey:@"fontSize"];
+    int fontSize = (fsv)? [fsv intValue] : 20;
     
-    NSDictionary *fontAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIFont fontWithName:@"Arial" size:40], NSFontAttributeName,[UIColor colorWithRed:255 green:255 blue:255 alpha:1],NSForegroundColorAttributeName, nil];
+    NSDictionary *shadowFontAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIFont fontWithName:@"Arial" size:fontSize], NSFontAttributeName,[UIColor colorWithRed:0 green:0 blue:0 alpha:1],NSForegroundColorAttributeName, nil];
     
-    UIGraphicsBeginImageContextWithOptions(inImage.size, false, inImage.scale);
-    [inImage drawInRect:CGRectMake(0, 0, inImage.size.width, inImage.size.height)];
+    NSDictionary *fontAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIFont fontWithName:@"Arial" size:fontSize], NSFontAttributeName,[UIColor colorWithRed:255 green:255 blue:255 alpha:1],NSForegroundColorAttributeName, nil];
+    
+    id mwv = [config objectForKey:@"outputMaxWidth"];
+    id mhv = [config objectForKey:@"outputMaxHeight"];
+    
+    int width = (mwv)?[mwv intValue]:inImage.size.width;
+    int height = (mhv)?[mhv intValue]:inImage.size.height;
+
+    if (mwv && mhv){
+        // scale
+        if (inImage.size.width > inImage.size.height){
+            height = (width * inImage.size.height) / inImage.size.width;
+        } else {
+            width = (height * inImage.size.width) / inImage.size.height;
+        }
+    }
+    
+    id qv = [config objectForKey:@"quality"];
+    float quality = (qv)?[qv floatValue]:.8;
+
+    
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(width, height), false, inImage.scale);
+    [inImage drawInRect:CGRectMake(0, 0, width, height)];
     [text drawAtPoint:CGPointMake(6, 6) withAttributes:shadowFontAttributes];
     [text drawAtPoint:CGPointMake(4, 4) withAttributes:fontAttributes];
 
     UIImage *annotatedImage = UIGraphicsGetImageFromCurrentImageContext();
     
-    NSData *outData = UIImageJPEGRepresentation(annotatedImage, 0.8);
+    NSData *outData = UIImageJPEGRepresentation(annotatedImage, quality);
     
     NSString *outImage64 = [outData base64EncodedStringWithOptions:0];
 
